cd28b985a6ef2175d7d41d9ee63a0891796333e7
Julian <julianrodriguezc@javeriana.edu.co>
2024-05-27 18:42:20 -0500

    Mascota Creada

--- app/src/main/java/com/example/lovemal/RegistrarMascota.kt
+++ app/src/main/java/com/example/lovemal/RegistrarMascota.kt
@@ -1,20 +1,31 @@
 package com.example.lovemal
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lovemal.databinding.ActivityRegistrarMascotaBinding
 
 class RegistrarMascota : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrarMascotaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_registrar_mascota)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegistrarMascotaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addPet()
+
+    }
+
+    private fun addPet(){
+        binding.btnNextStep.setOnClickListener {
+            var myPet = Pet()
+            myPet.nombre = binding.txtNombre.toString()
+            myPet.descripcion = binding.txtDescripcion.toString()
+            
         }
     }
 }
